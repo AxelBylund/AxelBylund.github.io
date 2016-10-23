72daee11ae4cd8d9fd9f12c57f4bbe5d227eeb74
AxelBylund <wa_gwa@hotmail.com>
2016-10-24 00:28:05 +0200

    Fixed visual que for swiping on mobile

--- src/style/javascript/main.js
+++ src/style/javascript/main.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function () {
   var element = document.getElementById('swipe');
   var hammertime = Hammer(element).on("swipeleft swiperight", function (event) {
+    $('#swipe').toggleClass('yellow');
     console.log(event.type);
   });
 });
