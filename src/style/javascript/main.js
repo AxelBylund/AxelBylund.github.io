235a91f5980b029648297a8dbb8e662caa877c0a
Axel Bylund <axel@bazooka.se>
2016-10-24 16:46:53 +0200

    animation stuff

--- src/style/javascript/main.js
+++ src/style/javascript/main.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function () {
   var element = document.getElementById('swipe');
   var hammertime = Hammer(element).on("swipeleft swiperight", function (event) {
-    $('#swipe').toggleClass('yellow');
+    $('#swipe').toggleClass('blue');
     console.log(event.type);
   });
 });
