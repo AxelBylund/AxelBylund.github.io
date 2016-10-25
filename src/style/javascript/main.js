cfb394374bc7e28643362efe478abce7520fcb4d
Axel Bylund <axel@bazooka.se>
2016-10-25 16:49:34 +0200

    added font-awsome & trying nav.vibrate on phone

--- src/style/javascript/main.js
+++ src/style/javascript/main.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function () {
   var element = document.getElementById('swipe');
   var hammertime = Hammer(element).on("swipeleft swiperight", function (event) {
+    navigator.vibrate([1000, 500, 1000]);
     $('#swipe').toggleClass('blue');
     console.log(event.type);
   });
