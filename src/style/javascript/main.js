b2026124669244d3fcb7d8ec1d43564c817ef03e
Axel Bylund <axel@bazooka.se>
2016-10-25 17:04:07 +0200

    fixed vibrate

--- src/style/javascript/main.js
+++ src/style/javascript/main.js
@@ -1,11 +1,8 @@
 window.addEventListener('load', function () {
   var element = document.getElementById('swipe');
   var hammertime = Hammer(element).on("swipeleft swiperight", function (event) {
-    if ("vibrate" in navigator) {
-		// vibration API supported 
-		navigator.vibrate(1000);
-	}
     $('#swipe').toggleClass('blue');
     console.log(event.type);
   });
 });
+
