345d0fd5d9d9cb8f3730b0c4241fa5b351efb3ba
Axel Bylund <axel@bazooka.se>
2016-10-25 16:58:50 +0200

    Second try of vibration

--- src/style/javascript/main.js
+++ src/style/javascript/main.js
@@ -1,7 +1,10 @@
 window.addEventListener('load', function () {
   var element = document.getElementById('swipe');
   var hammertime = Hammer(element).on("swipeleft swiperight", function (event) {
-    navigator.vibrate([1000, 500, 1000]);
+    if ("vibrate" in navigator) {
+		// vibration API supported 
+		navigator.vibrate(1000);
+	}
     $('#swipe').toggleClass('blue');
     console.log(event.type);
   });
