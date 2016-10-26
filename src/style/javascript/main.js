447040744f4623f1e3d9b0d7b7eaf3f17cf0d5d6
Axel Bylund <axel@bazooka.se>
2016-10-26 13:56:21 +0200

    added overlay'page' with glitch effect

--- src/style/javascript/main.js
+++ src/style/javascript/main.js
@@ -1,8 +1,17 @@
 window.addEventListener('load', function () {
-  var element = document.getElementById('swipe');
-  var hammertime = Hammer(element).on("swipeleft swiperight", function (event) {
-    $('#swipe').toggleClass('blue');
-    console.log(event.type);
-  });
+	var element = document.getElementById('swipe');
+	var hammertime = Hammer(element).on("swipeleft swiperight", function (event) {
+		$('#swipe').toggleClass('blue');
+		console.log(event.type);
+	});
 });
 
+$(document).ready(function() {
+	if ($('.overlay').is(":visible")) {
+		$('.navbar').hide();
+	};
+	$('.button-text').on('click', function() {
+		$('.overlay').fadeOut('3000');
+		$('.navbar').show();
+	})
+})
