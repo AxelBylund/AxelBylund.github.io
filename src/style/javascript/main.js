baa7bf2c6720a4dea6d7683d4454abcdaefed90a
Axel Bylund <axel@bazooka.se>
2016-11-11 15:16:11 +0100

    replaced SVG's with PNG's for CPU performance.

--- src/style/javascript/main.js
+++ src/style/javascript/main.js
@@ -1,17 +1,19 @@
 window.addEventListener('load', function () {
-	var element = document.getElementById('swipe');
-	var hammertime = Hammer(element).on("swipeleft swiperight", function (event) {
-		$('#swipe').toggleClass('blue');
-		console.log(event.type);
-	});
+    var element = document.getElementById('swipe');
+    var hammertime = Hammer(element).on("swipeleft swiperight", function (event) {
+        $('#swipe').toggleClass('blue');
+        console.log(event.type);
+    });
 });
-
-$(document).ready(function() {
-	if ($('.overlay').is(":visible")) {
-		$('.navbar').hide();
-	};
-	$('.button-text').click(function() {
-		$('.overlay').fadeOut('slow');
-		$('.navbar').show();
-	})
+$(document).ready(function () {
+    if ($('.overlay').is(":visible")) {
+        $('.navbar').hide();
+    };
+    $('.button-text').on('click', function () {
+        $('.overlay').fadeOut('slow');
+        $('.navbar').show();
+        $('.left').hide().fadeIn(2000);
+        $('.right').hide().fadeIn(5000);
+        console.log('asd');
+    });
 });
